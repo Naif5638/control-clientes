4d9328291172b18011bb1e4bbd6a13a91ce3fe86
Jonathan Lorenzo Contrera <naruto_rtx@hotmail.com>
2021-08-11 22:29:31 -0400

    Instalación de componentes a utilizar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    TableroComponent,
+    ClientesComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    RegistroComponent,
+    ConfiguracionComponent,
+    NotFoundComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
